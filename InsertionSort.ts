074f0822816a3c577e2688d75d5ab928cd1c52dc
Ashley Teo <thy.ashley@gmail.com>
2021-02-01 16:13:02 +0800

    added asc and desc function to insertionsort

--- InsertionSort.ts
+++ InsertionSort.ts
@@ -1,14 +1,23 @@
-function selectionSort(arr: number[]) {
+function selectionSort(arr: number[], mode: string ='asc') {
     for (let i=0; i < arr.length; i++) {
         let swap = false;
         let min = arr[i]
         let idx = i
         for (let j=1+i; j<arr.length; j++) {
-            if (arr[j] < min) {
-                min = arr[j]
-                idx = j
-                swap= true
+            if (mode === 'asc') {
+                if (arr[j] < min) {
+                    min = arr[j]
+                    idx = j
+                    swap= true
+                }
+            } else {
+                if (arr[j] > min) {
+                    min = arr[j]
+                    idx = j
+                    swap= true
+                }
             }
+
         }
         if (!swap) break;
         [arr[i], arr[idx]] = [arr[idx], arr[i]];
@@ -17,4 +26,4 @@ function selectionSort(arr: number[]) {
     return arr
 }
 
-selectionSort([3,1,2,4,4,6,-3,0]);
+selectionSort([3,1,2,4,4,6,-3,0], 'desc');
