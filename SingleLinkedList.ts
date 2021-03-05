23cf5dacf724f3188776757b3d49ce28003f244b
Ashley Teo <thy.ashley@gmail.com>
2021-03-05 15:32:46 +0800

    added set function

--- SingleLinkedList.ts
+++ SingleLinkedList.ts
@@ -7,7 +7,7 @@ class _Node {
     }
 }
 
-class SingleLinkedList{
+class SingleLinkedList {
     head: _Node | null;
     tail: _Node | null;
     length: number;
@@ -17,7 +17,7 @@ class SingleLinkedList{
         this.length = 0;
     }
 
-    push(val: any) : SingleLinkedList {
+    push(val: any): SingleLinkedList {
         let node = new _Node(val);
         if (!this.tail) {
             this.head = node;
@@ -26,20 +26,33 @@ class SingleLinkedList{
             this.tail.next = node;
             this.tail = node;
         }
-        
+
         this.length += 1;
         return this;
     }
-    shift(): SingleLinkedList {
-        let nextNode = this.head?.next;
-        if (nextNode) {
-            this.head = nextNode;
-            this.length--;
+
+    shift(): SingleLinkedList | null {
+        if (!this.head) return null;
+        var currentHead = this.head;
+        this.head = currentHead.next;
+        this.length--;
+        if (this.length == 0) {
+            this.tail = null;
+        }
+        return this;
+    }
+
+    unshift(val: any): SingleLinkedList {
+        if (!this.head) {
+            this.push(val);
             return this;
         }
-        this.head = null;
-        this.tail = null;
-        this.length--;
+        let node = new _Node(val);
+        let currentHead = this.head;
+        this.head = node;
+        this.head.next = currentHead
+        this.length++;
+
         return this;
     }
 
@@ -51,28 +64,45 @@ class SingleLinkedList{
             this.length = 0;
             return this;
         }
-        while(current) {
+        while (current) {
             if (current.next == this.tail) {
                 this.tail = current;
                 current.next = null;
                 this.length--;
                 break;
-                
+
             }
             current = current.next;
 
         }
         return this;
     }
+
+    get(val: number): (_Node | null) {
+        if (this.length < val || val < 0) return null;
+        if (val === 1) return this.head!.next;
+        if (val === 0) return this.head;
+        let count = 0;
+        let node = this.head;
+        while (count !== val) {
+            node = node!.next;
+            count++;
+        }
+        return node;
+    }
+
+    set(val: any, index: number): SingleLinkedList | null {
+        const node = this.get(index);
+        if (!node) return null;
+        node.val = val;
+        return this;
+    }
 }
+    
 
 let list = new SingleLinkedList();
-list.push("Hey");
-list.push("There");
-list.push("Test");
-list.pop();
-console.log(list);
-list.shift();
-console.log(list);
-list.shift();
-console.log(list);
+list.push("123");
+list.push("1234");
+list.push("1235");
+list.push("bye");
+console.log(list.set(1,5));
