d76e08fef5f0e7d3f6dbe351ecc9b9d082120d83
Ashley <thy.ashley@gmail.com>
2021-03-09 12:42:23 +0800

    added hacker rank algos

---
+++ HackerRank_ArrayManipulation.ts
@@ -0,0 +1,17 @@
+
+function ArrayManipulation(n: number, queries: number[][]) {
+    let max = -Infinity;
+    let arr = Array.from({length: n}, () => 0);
+    queries.forEach((query) => {
+        let start = query[0];
+        let end = query[1];
+        let value = query[2];
+        for (let i = start - 1; i < end; i++){
+            arr[i] += value;
+            max = Math.max(arr[i], max);
+        }
+    })
+    return max;
+}
+
+ArrayManipulation(10, [[1,5,3],[4,8,7],[6,9,1]]);
