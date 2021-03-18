e2dd9b8d4f792cc81c2be2265020a5e1ce213ed6
thyashley <thy.ashley@gmail.com>
2021-03-18 22:14:02 +0800

    added change maker

---
+++ ChangeMaker.ts
@@ -0,0 +1,24 @@
+function ChangeMaker(availableCoin: number[], target: number) {
+    let change: {[key: number] : number} = {}
+    let remainder = target;
+    let numberOfCoin = 0;
+
+    for (let coin of availableCoin) {
+        change[coin] = 0;
+    }
+
+    for (let coin of availableCoin) {
+        if (remainder === 0) break;
+        let newRemainder = remainder % coin;
+        change[coin] = (remainder - newRemainder) / coin
+        remainder = newRemainder
+        numberOfCoin += change[coin]
+    }
+    console.log(numberOfCoin)
+
+}
+
+const target = 129;
+const availableCoin = [100,50,20,10,5,2,1];
+
+ChangeMaker(availableCoin, target);
