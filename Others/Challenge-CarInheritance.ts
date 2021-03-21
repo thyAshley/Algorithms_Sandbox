414077e37c273fce91908b774bd15974cd5ef6ae
thyashley <thy.ashley@gmail.com>
2021-03-21 15:10:58 +0800

    added car inheritance.ts

---
+++ Others/Challenge-CarInheritance.ts
@@ -0,0 +1,56 @@
+class Car {
+    protected isSedan: boolean;
+    protected seats: number;  
+    protected mileage: number;
+  
+    constructor(isSedan: boolean, seats: number) {
+      this.isSedan = isSedan;
+      this.seats = seats;
+      this.mileage = 0;
+    }
+  
+    public getIsSedan() {
+      return this.isSedan
+    }
+  
+    public getSeats() {
+      return this.getSeats
+    }
+  }
+  
+  class WagonR extends Car{
+    constructor(mileage: number) {
+      super(true, 4);
+      this.mileage = mileage
+    }
+  
+    getMileage() {
+      return `${this.mileage} kmpl`
+    }
+  }
+  
+  class HondaCity extends Car{
+    constructor(mileage: number) {
+      super(true, 6);
+      this.mileage = mileage
+    }
+  
+    getMileage() {
+      return `${this.mileage} kmpl`
+    }
+  }
+  
+  class InnovaCrysta extends Car{
+    constructor(mileage: number) {
+      super(false, 4);
+      this.mileage = mileage
+    }
+  
+    getMileage() {
+      return `${this.mileage} kmpl`
+    }
+  }
+  
+  
+  const wagon = new WagonR(120);
+  console.log(wagon.getMileage());
